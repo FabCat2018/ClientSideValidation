5b364ec1ee9c843e3af201ef629515b7c1fb1caa
Fabio Cataleta <fcataleta@gmail.com>
2020-05-07 15:13:57 +0100

    Added Client-Side Validation example

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using ClientSideValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientSideValidation.Controllers
+{
+	public class UsersController : Controller
+	{
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(WebUser webUser)
+		{
+			if(ModelState.IsValid) {
+				// Here the WebUser should be saved. Afterwards we would normally return
+				// another View, to indicate that the User has been sucessfully created, or
+				// redirect to another page
+			}
+
+			return View();
+		}
+	}
+}
---
+++ Models/WebUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientSideValidation.Models
+{
+	public class WebUser
+	{
+		[Required]
+		[StringLength(25)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 3)]
+		public string LastName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string MailAddress { get; set; }
+	}
+}
